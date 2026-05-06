d7d6c4ab6ff0e5bbf688fa3a0e97564725a20d84
pattypatxxr <113111460+pattypatxxr@users.noreply.github.com>
2026-05-06 14:15:57 +0700

    Modify YOLO training configuration
    Updated training parameters for YOLO model including increased epochs, batch size, and added dropout.

--- train_yolo.py
+++ train_yolo.py
@@ -1,16 +1,17 @@
 from ultralytics import YOLO
 
-# โหลด YOLO11 classification model (pretrained)
-model = YOLO("yolo11n-cls.pt")  # n = nano (เบาสุด เร็วสุด)
+# YOLO11 classification model (pretrained)
+model = YOLO("yolo11n-cls.pt")  
 
 # Train
 results = model.train(
-    data="lemon-leaf-disease-1/",   # path ที่ได้จาก Roboflow
-    epochs=30,
-    imgsz=224,
-    batch=32,
-    name="lemon_disease_cls",
-    patience=10        # early stopping
-)
+    data="lemon_leaf-disease-1/",   # path Roboflow
+        epochs=40,
+        imgsz=224,
+        batch=16,
+        dropout=0.3,       
+        patience=15, # early stopping
+)       
+
 
 print("Training complete!")
